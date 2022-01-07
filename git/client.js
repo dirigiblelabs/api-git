dd9f2c2813fcb0521dc26f9cd830551363443351
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-01-07 11:50:24 +0200

    rename index to client

--- git/client.js
+++ git/client.js
@@ -9,7 +9,7 @@
  * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-var git = require('git/v4');
+var git = require('git/v4/client');
 for(var propertyName in git) {
 	exports[propertyName] = git[propertyName];
 }
